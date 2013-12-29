f6a8de564a0ad9fdf80c71884cfd7a39c5f0b121
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-12-29 11:12:02 -0600

    Initial Commit

---
+++ LearningiOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LearningiOS
+//
+//  Created by Joshua Hall  on 12/29/13.
+//  Copyright (c) 2013 Queue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
