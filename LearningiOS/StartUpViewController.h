ee242188308911fcc0fb593a578a234e022948ea
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-12-29 11:26:26 -0600

    created inital ViewController

---
+++ LearningiOS/StartUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  StartUpViewController.h
+//  LearningiOS
+//
+//  Created by Joshua Hall  on 12/29/13.
+//  Copyright (c) 2013 Queue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartUpViewController : UIViewController
+
+@end
---
+++ LearningiOS/StartUpViewController.m
@@ -0,0 +1,44 @@
+//
+//  StartUpViewController.m
+//  LearningiOS
+//
+//  Created by Joshua Hall  on 12/29/13.
+//  Copyright (c) 2013 Queue. All rights reserved.
+//
+
+#import "StartUpViewController.h"
+
+@interface StartUpViewController ()
+
+@end
+
+@implementation StartUpViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+//    UIView *mainView = self.view;
+//    UIView *v = [[UIView alloc]initWithFrame:CGRectMake(100, 100, 50, 50)];
+//    [v setBackgroundColor:[UIColor redColor]];
+//    [mainView addSubview:v];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
